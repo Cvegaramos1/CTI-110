f2b6773a4e65b35e7d14bd348c8b377be96e000a
Cvegaramos1 <vegaramc0848@student.faytechcc.edu>
2024-06-20 21:40:54 -0400

    Add files via upload

---
+++ P1LAB_VegaCarlos.py
@@ -0,0 +1,8 @@
+ # Carlos Vega
+ # 6/18/2024
+ # P1LAB1
+ # This project is to welcome new users tto CTI-110
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+print("Hello,", first_name, last_name+"!", "Welcome to CTI-110")
