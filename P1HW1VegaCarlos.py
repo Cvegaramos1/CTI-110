ceb423d5c9d4e734149cfa8beae881717b13b71e
Cvegaramos1 <vegaramc0848@student.faytechcc.edu>
2024-07-02 13:11:17 -0400

    Add files via upload

---
+++ P1HW1VegaCarlos.py
@@ -0,0 +1,34 @@
+# Carlos Vega
+# 6/23/2024
+# P1HW1
+# This program helps with calculating exponents
+
+
+print("-----Calculating Exponents-----")
+print()
+
+base = int(input("Enter an integar as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+result = base ** exponent
+
+print()
+print()
+
+print(f"{base} raised to the power of {exponent} is {result} !!")
+
+print()
+print()
+
+print("-----Addition and Subtraction-----")
+
+print()
+print()
+
+integer = int(input("Enter a starting integer: "))
+add =int(input("Enter an interger to add: "))
+subtract =int(input("Enter an integer to subtract: "))
+result = integer + add - subtract
+print()
+print()
+
+print (f"{integer} + {add} - {subtract} is equal to {result}")
